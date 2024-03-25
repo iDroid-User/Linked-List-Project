5c103557336e9d1fb2df4aa2d64487eaf5ad1a86
iDroid-User <steventejada6@gmail.com>
2024-03-24 18:01:44 -0700

    Cleaned up comments, getters, and setters.

--- Linked List Project/LetterFrequency.h
+++ Linked List Project/LetterFrequency.h
@@ -4,89 +4,79 @@
 
 using namespace std;
 
-// Add your both class definitions here
-class LetterNode { // Defined according to UML diagram
+// Defined according to UML diagram
+class LetterNode {
     public:
-        LetterNode(char chr) { //Construct a LetterNode containing the given character
+        LetterNode(char chr) { // Construct a LetterNode containing the given character
             letter = chr;
-            frequency = 1; //If previously unknown, this letter now has a frequency of 1!
-            next = nullptr; //Initializes the "next" pointer
+            frequency = 1; // If previously unknown, this letter now has a frequency of 1!
+            next = nullptr; // Initializes the "next" pointer
         }
 
-        ~LetterNode() { //Complete; just prints to cout
-            cout << "Destroying " << letter << "\n";
+        ~LetterNode() {
+            cout << "Destroying " << letter << " ...\n"; // Destructor message
         }
 
-        void increaseFrequency() {
-            frequency++;
-        }
+        void increaseFrequency() {frequency++;}
 
-        char getLetter() {
-            return letter;
-        }
+        char getLetter() {return letter;}
 
-        int getFrequency() {
-            return frequency;
-        }
+        int getFrequency() {return frequency;}
 
-        LetterNode *getNext() {
-            return next;
-        }
+        LetterNode *getNext() {return next;}
 
-        void setNext(LetterNode *next) {
-            this->next = next;
-        }
+        void setNext(LetterNode *next) {this->next = next;}
 
     private:
-        int frequency; //The number of times that the letter has been read in
-        char letter; //The alphabetic letter that this node represents (node identifier)
-        LetterNode *next; //The pointer to the next node in the list
+        int frequency; // The number of times that the letter has been read in
+        char letter; // The alphabetic letter that this node represents (node identifier)
+        LetterNode *next; // The pointer to the next node in the list
 };
 
-class LetterFrequencyList { // Not yet Defined according to UML diagram
+class LetterFrequencyList {
     public:
         LetterFrequencyList() {
-            head = nullptr; //Initializes the list
+            head = nullptr; // Initializes the list
         }
 
         ~LetterFrequencyList() {
-            //Add a recursive function that deletes from the END of the list to the FRONT
+            // Add a recursive function that deletes from the END of the list to the FRONT
             delist(head);
 
             cout << "Done destroying list\n";
             head = nullptr;
         }
 
-        bool append(char letter) { //Optimized.
-            LetterNode *foundNode = find(letter); //A "getter" traversal of the list (traverses to get, not set)
-            //If the node ISN'T in the list
+        bool append(char letter) { // Optimized.
+            LetterNode *foundNode = find(letter); // A "getter" traversal of the list (traverses to get, not set)
+            // If the node ISN'T in the list
             if (nullptr == foundNode) {
-                head = new LetterNode(letter); //Appends the first node
-                return true; //Indicates that a new node was added
+                head = new LetterNode(letter); // Appends the first node
+                return true; // Indicates that a new node was added
             }
             else if (foundNode->getLetter() != letter) {
-                foundNode->setNext(new LetterNode(letter)); //Appends to derived insertion point
+                foundNode->setNext(new LetterNode(letter)); // Appends to derived insertion point
                 return true;
             }
-            //If the node IS on the list
+            // If the node IS on the list
             else {
                 foundNode->increaseFrequency();
-                return false; //Indicates that a new node wasn't added
+                return false; // Indicates that a new node wasn't added
             }
         }
 
         bool insertInOrder(char letter) {
             LetterNode *foundNode = find(letter);
-            LetterNode *amazon = nullptr; //Keeps it A to Z, like Amazon
-            //If the node ISN'T in the list
-            //Add the node to the list so that the list is maintained in alphabetical order
+            LetterNode *amazon = nullptr; // Keeps it A to Z, like Amazon
+            // If the node ISN'T in the list
+            // Add the node to the list so that the list is maintained in alphabetical order
             if (nullptr == foundNode) {
-                head = new LetterNode(letter); //Inserts the first node of the sorted list
-                return true; //Indicates that a new node was added
+                head = new LetterNode(letter); // Inserts the first node of the sorted list
+                return true; // Indicates that a new node was added
             }
             else if (foundNode->getLetter() != letter) {
                 if (letter > foundNode->getLetter()) {
-                    if (foundNode->getNext() == nullptr) { //Worst-case scenario, the last node is returned to foundNode
+                    if (foundNode->getNext() == nullptr) { // Worst-case scenario, the last node is returned to foundNode
                         foundNode->setNext(new LetterNode(letter));
                         return true;
                     }
@@ -97,51 +87,51 @@ class LetterFrequencyList { // Not yet Defined according to UML diagram
                 }
                 else {
                     LetterNode *orderHere = alphabetize(letter);
-                    if (nullptr == orderHere) { //New head
+                    if (nullptr == orderHere) { // New head
                         return true;
                     }
                     else if (orderHere->getNext() == nullptr) {
-                        orderHere->setNext(new LetterNode(letter)); //New top letter
+                        orderHere->setNext(new LetterNode(letter)); // New top letter
                         return true;
                     }
                     else {
                         amazon = orderHere->getNext();
                         orderHere->setNext(new LetterNode(letter));
-                        orderHere->getNext()->setNext(amazon); //Insertion in the middle
+                        orderHere->getNext()->setNext(amazon); // Insertion in the middle
                         return true;
                     }
                 }
             }
-            //If the node IS on the list
+            // If the node IS on the list
             else {
                 foundNode->increaseFrequency();
-                return false; //Indicates that a new node wasn't added
+                return false; // Indicates that a new node wasn't added
             }
         }
 
         int getFrequency(char letter) {
-            //Given a capitalized letter
+            // Given a capitalized letter
             LetterNode *foundNode = find(letter);
-            //If the letter ISN'T in the list
+            // If the letter ISN'T in the list
             if (nullptr == foundNode || foundNode->getLetter() != letter) {
                 return 0;
             } 
             return foundNode->getFrequency();
         }
 
-        bool remove(char letter) { //Complete
+        bool remove(char letter) { // Complete
             LetterNode *removeHere = head;
-            //If the letter is found
+            // If the letter is found
             while (removeHere->getNext() != nullptr) {
                 if (removeHere->getNext()->getLetter() == letter) {
                     LetterNode *tmp = removeHere->getNext();
-                    removeHere->setNext(removeHere->getNext()->getNext()); //Fills the gap
+                    removeHere->setNext(removeHere->getNext()->getNext()); // Fills the gap
                     tmp->~LetterNode();
-                    return true; //The letter was found and removed
+                    return true; // The letter was found and removed
                 }
                     removeHere = removeHere->getNext();
             }
-            //If the letter ISN'T in the list
+            // If the letter ISN'T in the list
             return false;
         }
 
@@ -159,10 +149,10 @@ class LetterFrequencyList { // Not yet Defined according to UML diagram
             }
         }
 
-        LetterNode *find(char letter) { //Completed
-            //Given a capitalized letter
+        LetterNode *find(char letter) { // Completed
+            // Given a capitalized letter
             if (nullptr == head) {
-                return nullptr; //Empty list
+                return nullptr; // Empty list
             }
             else if (head->getLetter() == letter) {
                 return head;
@@ -171,19 +161,19 @@ class LetterFrequencyList { // Not yet Defined according to UML diagram
                 LetterNode *lookHere = head;
                 while (lookHere->getNext() != nullptr) {
                     if (lookHere->getLetter() == letter) {
-                        return lookHere; //Returns match
+                        return lookHere; // Returns match
                     }
                     lookHere = lookHere->getNext();
                 }
-                return lookHere; //Letter not found; returns insertion point | May conflict w/ sorted list
+                return lookHere; // Letter not found; returns insertion point (may conflict w/ sorted list)
             }
         }
 
     private:
-        LetterNode *head; //The initial node
-        //LetterNode *foundNode; //Saves a found letter node [one call to find() per class instance]
+        LetterNode *head; // The initial node
+        // LetterNode *foundNode; // Saves a found letter node [one call to find() per class instance]
 
-        void delist(LetterNode *node) { //Helper function, deletes nodes from END to FRONT recursively
+        void delist(LetterNode *node) { // Helper function, deletes nodes from END to FRONT recursively
             if (nullptr == node) {
                 return;
             }
@@ -195,7 +185,7 @@ class LetterFrequencyList { // Not yet Defined according to UML diagram
 
         LetterNode *alphabetize(char letter) {
             LetterNode *alphaBet = head;
-            LetterNode *preNode = nullptr; //Saves the previous node
+            LetterNode *preNode = nullptr; // Saves the previous node
             LetterNode *amazon = nullptr;
             while (alphaBet != nullptr) {
                 if (letter > alphaBet->getLetter()) {
@@ -206,12 +196,12 @@ class LetterFrequencyList { // Not yet Defined according to UML diagram
                     amazon = new LetterNode(letter);
                     amazon->setNext(head);
                     head = amazon;
-                    return nullptr; //The helper function helped, so there's nothing to return
+                    return nullptr; // The helper function helped, so there's nothing to return
                 }
-                else { //letter < alphaBet
+                else { // letter < alphaBet
                     return preNode;
                 }
             }
-            return preNode; //New top letter
+            return preNode; // New top letter
         }
 };
